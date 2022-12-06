f815e995f38411f643561b8c12c24409bf073cfb
rafidoth <asdarafi@gmail.com>
2022-12-06 19:34:24 +0600

    regular expression, final submit screen added

--- script.js
+++ script.js
@@ -35,74 +35,21 @@ nightModeBtn.addEventListener("click",modeChange);
 
 
 
-
-
 //form control
 const mainForm = document.getElementById("main-form");
 
-const step1 = `
-<div class="step1">
-<h1 style="font-size: 40px;">Personal Info</h1>
-<p class="sub-heading" style="font-size: 18px;">Please provide your name, email address and phone number</p>
-<div class="form-input">
-    <p>Name</p>
-    <input placeholder="e.g. Rafiul Hasan" type="text">
-</div>
-
-<div class="form-input">
-    <p>Email Address</p>
-    <input placeholder="e.g. rafiulhasan@lorem.com" type="text">
-</div>
-
-<div class="form-input">
-    <p>Phone Number</p>
-    <input placeholder="e.g. 017********" type="text">
-</div>
-</div>
-`
-
-const step2 = `
-<div class="step2">
-<h1 style="font-size: 40px;">EDUCATIONAL Info</h1>
-<p class="sub-heading" style="font-size: 18px;">Please provide your university name and ID</p>
-<div class="form-input">
-    <p>University Name</p>
-    <input placeholder="e.g. United International University" type="text">
-</div>
-
-<div class="form-input">
-    <p>ID</p>
-    <input placeholder="e.g. 12345678910" type="text">
-</div>
-
-</div>
-<div class="last-row">
-<button class="hover next-btn">Next</button>
-</div>
-`
-
-
-const step3 = `
-<div class="step3">
-    <h1 style="font-size: 40px;">Your Comment</h1>
-    <p class="sub-heading" style="font-size: 18px;">Please provide your comment here</p>
-    <div class="form-input">
-        <p>Write here</p>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-    </div>
-</div>
-<div class="last-row">
-<button class="hover">Submit</button>
-</div>
-`
-list_of_steps = [step1, step2, step3];
+const step1 = document.getElementById('step1');
+const step2 = document.getElementById('step2');
+const step3 = document.getElementById('step3');
 
 const s1 = document.getElementById('1');
 const showStep1 = () =>{
     s3.classList.remove('num-focused');
     s2.classList.remove('num-focused');
     s1.classList.add('num-focused')
-    return mainForm.innerHTML = step1;
+    step1.classList.remove("hide");
+    step2.classList.add("hide");
+    step3.classList.add("hide");
 };
 s1.addEventListener('click',showStep1);
 
@@ -112,7 +59,9 @@ const showStep2 = () =>{
     s1.classList.remove('num-focused');
     s3.classList.remove('num-focused');
     s2.classList.add('num-focused')
-    return mainForm.innerHTML = step2;
+    step2.classList.remove("hide");
+    step1.classList.add("hide");
+    step3.classList.add("hide");
 };
 s2.addEventListener('click',showStep2);
 
@@ -122,9 +71,119 @@ const showStep3 = () =>{
     s1.classList.remove('num-focused');
     s2.classList.remove('num-focused');
     s3.classList.add('num-focused');
-    mainForm.innerHTML = step3;
+    step3.classList.remove("hide");
+    step1.classList.add("hide");
+    step2.classList.add("hide");
 };
 s3.addEventListener('click',showStep3);
 
 
-const nextBtn = document.getElementById('
+const next1 = () =>{
+    nameValidate() && emailValidate() &&phoneValidate() ? showStep2() : 1;
+};
+
+const next2 = () =>{
+    uniValidate() && idValidate() ? showStep3() : 1;
+}
+const nextBtnPI = document.getElementById('next-pi');
+nextBtnPI.addEventListener('click',next1);
+const nextBtnEI = document.getElementById('next-yc');
+nextBtnEI.addEventListener('click', next2);
+
+
+
+
+// form validation
+const nameValidate = () =>{
+    const  name = document.getElementById('name').value;
+    const error = document.getElementById('name-error');
+    const pattern = /^[a-zA-Z\s]+$/;
+    if(name.match(pattern)){
+        error.innerText = " ";
+        return true;
+
+    }else{
+        error.innerText = "Please enter a valid name";
+        return false;
+    }
+}
+
+
+const emailValidate = () =>{
+    const  email = document.getElementById('email').value;
+    const error = document.getElementById('email-error');
+    const pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(email.match(pattern)){
+        error.innerText = " ";
+        return true;
+
+    }else{
+        error.innerText = "Please enter a valid email address";
+        return false;
+    }
+}
+
+
+const phoneValidate = () =>{
+    const  phone = document.getElementById('phone').value;
+    const error = document.getElementById('phone-error');
+    const pattern =  /(^(\+8801|8801|01|008801))[1|3-9]{1}(\d){8}$/;
+    if(phone.match(pattern)){
+        error.innerText = " ";
+        return true;
+
+    }else{
+        error.innerText = "Please enter a valid phone address";
+        return false;
+    }
+}
+
+
+const uniValidate = () =>{
+    const  uni = document.getElementById('uni').value;
+    const error = document.getElementById('uni-error');
+    const pattern =  /^[a-zA-Z\s]+$/;
+    if(uni.match(pattern)){
+        error.innerText = " ";
+        return true;
+
+    }else{
+        error.innerText = "Please enter a valid university name";
+        return false;
+    }
+}
+
+const idValidate = () =>{
+    const  id = document.getElementById('id').value;
+    const error = document.getElementById('id-error');
+    const pattern =  /^\d+$/;
+    if(id.match(pattern)){
+        error.innerText = " ";
+        return true;
+
+    }else{
+        error.innerText = "Please enter a valid ID";
+        return false;
+    }
+}
+
+
+
+
+// after submittiing the form
+const finalSubmit = document.getElementById('final-submit');
+
+const submitHandler  = () =>{
+    const container = document.getElementsByClassName('container');
+    const name = document.getElementById('name').value;
+    container[0].classList.add('centerize');
+    const thankyou = `
+    <div>
+        <h1>Thank You, ${name}</h1>
+        <p>for providing these informations</p>
+    </div>
+    `;
+    container[0].innerHTML = thankyou;
+    console.log(container.classList(), container);
+}
+finalSubmit.addEventListener('click', submitHandler);
